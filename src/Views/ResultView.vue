3fbdf5be373923cbf71de3778a412e84d3cec6cc
Pei-Han <peihan945@gmail.com>
2024-05-07 12:53:04 +0800

    popover finish

--- src/Views/ResultView.vue
+++ src/Views/ResultView.vue
@@ -26,24 +26,42 @@
   </div>
   <div class="content-container">
     <div class="content">
+      <div>
       <n-scrollbar style="height: 450px">
-        <div>
-            <n-tooltip trigger="hover">
-            <template #trigger>
-            加油站不可告人的三大秘密
-            第一點: 早上和晚上的油最便宜
-            因為熱脹冷縮的原理，這個時候汽油的質量最大。所以花同樣的錢，結果加的油卻變多了
-            第二點: 加油的時候一定要跟加油的人員說
-            加多少升而不是加多少錢，因為四捨五入之後，吃虧的還是你自己喔。
-            第三點: 加完油之後不要說湊個整數    
-            因為此時湊整數裡面的油，都是輸油管裡面的空氣，等於加了個寂寞
-        </template>
-    If it looks like a duck, walks like a duck, and quacks like a duck...it must
-    be a duck.
-  </n-tooltip>
-        </div>
+        加油站不可告人的三大秘密
+        第一點: 早上和晚上的油最便宜
+        <n-popover trigger="hover">
+           <template #trigger>
+            <n-button class="sus" :bordered=false :text=true>熱脹冷縮的原理</n-button> 
+           </template>
+          <span>根據工研院的研究，環境溫度幾乎不影響加油機的準確性，分別在高溫（30℃以上）及低溫 <br/>
+            （20℃以下）環境下所測得的油品體積量值差距，只有0.055%，其差異尚在加油<br/>
+            機應有準確度（0.5%）的許可範圍內</span>
+        </n-popover>
+
+        ，這個時候汽油的質量最大。所以花同樣的錢，結果加的油卻變多了
+        第二點: 加油的時候一定要跟加油的人員說
+        加多少升而不是加多少錢，因為
+        <n-popover trigger="hover">
+           <template #trigger>
+             <n-button class="sus" :bordered=false :text=true>四捨五入之後</n-button> 
+           </template>
+           <span>現行加油站的加油機都是電子式加油機，其加油數量計量到小數點第2位</span>
+        </n-popover>
+
+        ，吃虧的還是你自己喔。
+        第三點: 加完油之後不要說湊個整數
+        因為此時湊整數裡面的油，
+        <n-popover trigger="hover">
+           <template #trigger>
+             <n-button class="sus" :bordered=false :text=true>都是輸油管裡面的空氣</n-button> 
+           </template>
+           <span>加油湊整數金額時，加進的仍然是油，並非空氣</span>
+        </n-popover>
+        ，等於加了個寂寞
 
       </n-scrollbar>
+      </div>
     </div>
   </div>
 
@@ -53,7 +71,7 @@
 </template>
 
 <script setup>
-import { NButton, NProgress, NScrollbar, NTooltip } from "naive-ui";
+import { NButton, NProgress, NScrollbar, NPopover } from "naive-ui";
 </script>
 
 <style scoped>
@@ -89,7 +107,7 @@ import { NButton, NProgress, NScrollbar, NTooltip } from "naive-ui";
 .content {
   display: flex;
   justify-content: center;
-  max-width: 97%;
+  width: 97%;
   max-height: 97%;
   align-items: center;
   margin: 0 auto;
@@ -112,4 +130,10 @@ import { NButton, NProgress, NScrollbar, NTooltip } from "naive-ui";
   margin: 20px 30px;
   font-size: 1.2rem;
 }
+
+.sus {
+  font-size: 1.2rem;
+  color: red;
+}
+
 </style>
